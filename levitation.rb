decde56d119c419f0b8dee50859ef35f4b25736c
shaileygulrajani <shaileygulrajani@gmail.com>
2018-07-24 20:16:12 +0000

    automatically backed up by learn

---
+++ levitation.rb
@@ -0,0 +1,14 @@
+def counter 
+levitation = 6 
+while levitation < 10
+puts "Wingardium Leviosa"
+levitation +=1
+end 
+end 
+counter
+
+def counter 
+levitation = 6 
+
+end 
+counter
--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
 def using_until
         levitation_force = 6
-        #your code here
+        until levitation == 10
+puts "Wingardium Leviosa"
+levitation +=1
+end 
     
 end
-
+using_until
